1b1f143ad7b43eef1ef727d0aae49c96ff45d5ac
Ainsley Rutterford <ainsley.rutterford@gmail.com>
2024-03-03 18:40:06 +0000

    Add working LLRT Lambda, enable NX, and rename service package to infra (#3)

---
+++ packages/infra/lib/stack.ts
@@ -0,0 +1,30 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+
+import { LlrtFunction } from 'cdk-lambda-llrt';
+import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
+
+export class TinyMuStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const testBucket = new s3.Bucket(this, 'MyFirstBucket', { versioned: true, removalPolicy: cdk.RemovalPolicy.DESTROY });
+
+    const testHandler = new LlrtFunction(this, 'TestHandler', {
+      entry: '../lambda/index.ts',
+      functionName: this.createResourceName('TestHandler'),
+    });
+
+    testHandler.addToRolePolicy(
+      new PolicyStatement({
+        actions: ['s3:ListAllMyBuckets'],
+        resources: ['*'],
+      })
+    );
+  }
+
+  createResourceName(suffix: string) {
+    return `${this.stackName}-${suffix}`;
+  }
+}
--- packages/service/src/index.ts
+++
@@ -1,7 +0,0 @@
-const createShortUrl = (longUrl: string) => {
-};
-
-const serveLongUrlRedirect = (shortUrl: string) => {
-  // Fetch the long URL from the cache (Redis) or DynamoDB. Look into types of caching for DynamoDB. (Look out for pricing!)
-  // Return a 302 response to the long URL
-};
