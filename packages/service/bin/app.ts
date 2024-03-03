9b85e07112ce2cfa863a77c3677713a26cc55ecb
Ainsley Rutterford <ainsley.rutterford@gmail.com>
2024-03-03 16:52:25 +0000

    Add GitHub CDK diff and deploy actions

--- packages/service/bin/app.ts
+++ packages/service/bin/app.ts
@@ -3,9 +3,13 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { TinyMuStack } from '../lib/stack';
 
+const ACCOUNT_ID = '381492110289';
+const REGION = 'eu-west-2';
+
 const app = new cdk.App();
 
-new TinyMuStack(app, 'TinyMuStack', {
+// Stack used for development and testing
+new TinyMuStack(app, 'TinyMu-dev', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
@@ -16,7 +20,10 @@ new TinyMuStack(app, 'TinyMuStack', {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+  env: { account: ACCOUNT_ID, region: REGION },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
+
+// The prod stack that the GitHub action deploys to
+new TinyMuStack(app, 'TinyMu-prod', { env: { account: ACCOUNT_ID, region: REGION } });
