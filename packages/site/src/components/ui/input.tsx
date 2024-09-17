37887df79354c505b0a67a829c7396e567a6246e
Ainsley Rutterford <ainsley.rutterford@gmail.com>
2024-09-18 00:28:07 +0100

    Replace readonly inputs with divs

--- packages/site/src/components/ui/input.tsx
+++ packages/site/src/components/ui/input.tsx
@@ -22,4 +22,20 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
 )
 Input.displayName = "Input"
 
-export { Input }
+/**
+ * This is a `<div>` that we style as an `<input>` since the readonly input had an issue on iOS
+ * that I couldn't fix. When you tapped the input to focus it, the autofill and share menu
+ * would pop up at the top of the screen instead of over the input. There was a StackOverflow
+ * about it but nothing would work: https://stackoverflow.com/q/70199584
+ * 
+ * Also, on Firefox desktop: selecting text in the input, clicking off the input, and clicking
+ * back on would cause the selection to reappear for a second before disappearing which didn't
+ * look nice at all. Both of these issues happened with an unstyled `<input>` tag, so I've
+ * just gone with a `<div>` instead.
+ */
+const ReadOnlyInput = ({ children }: { children: React.ReactNode }) =>
+  <div className="flex items-center h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50">
+    {children}
+  </div>;
+
+export { Input, ReadOnlyInput }
