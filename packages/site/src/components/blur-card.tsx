d15b239c660857fb3e9bad9105174018007469e9
Ainsley Rutterford <ainsley.rutterford@gmail.com>
2025-04-08 23:06:59 +0100

    Remove login page from prod

--- packages/site/src/components/blur-card.tsx
+++ packages/site/src/components/blur-card.tsx
@@ -1,10 +1,8 @@
 import React from "react";
 import { Card } from "./ui/card";
-import { useTheme } from "next-themes";
 
 const BlurCard = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
   ({ className, ...props }, ref) => {
-    const { resolvedTheme } = useTheme();
     return (
       <Card ref={ref} className="bg-white/45 border-none shadow-none" {...props}>
         {props.children}
